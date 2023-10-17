d79cfa53340f4648da8a1cf8e7ce816f3b8faadf
jeffplata <jeffplata@yahoo.com>
2023-10-17 21:50:43 +0800

    start adding flask-security

--- app/models.py
+++ app/models.py
@@ -1,10 +1,23 @@
 from datetime import datetime
 from time import time
-from flask_login import UserMixin
+# from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 import jwt
 from flask import current_app
+from flask_security import UserMixin, RoleMixin
+
+
+user_roles = db.Table('user_roles',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+                      )
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
 
 
 class User(UserMixin, db.Model):
@@ -13,8 +26,12 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(64), index=True)
     last_name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
     password_hash = db.Column(db.String(128))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    roles = db.relationship('Role', secondary=user_roles,
+                            backref=db.backref('users', lazy='dynamic'))
 
     # messages_sent = db.relationship('Message',
     #                                 foreign_keys='Message.sender_id',
